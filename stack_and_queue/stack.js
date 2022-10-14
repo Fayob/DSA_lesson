3972a2c8342960dfc4bb46eddc2318ec1f08a603
= <adedejifavour6@gmail.com>
2022-10-14 15:39:51 +0100

    Removed stack with array functionality

--- stack_and_queue/stack.js
+++ stack_and_queue/stack.js
@@ -5,6 +5,7 @@ class Node {
   }
 }
 
+// Stack with linked list
 class Stack {
   constructor() {
     this.top = null;
@@ -50,44 +51,11 @@ class Stack {
   }
 }
 
-// Stack used with Array
-class StackArray {
-  constructor() {
-    this.array = []
-  }
-
-  peek() {
-    if (this.array.length === 0) {
-      return null
-    }
-    return this.array[this.array.length - 1]
-  }
-
-  push(value) {
-    this.array.push(value)
-    return this
-  }
-
-  pop() {
-    this.array.pop()
-    return this
-  }
-}
-
-const stackWithArray = new StackArray()
-console.log(stackWithArray.push('google')); 
-console.log(stackWithArray.push('udemy')); 
-console.log(stackWithArray.push('amazon')); 
-console.log(stackWithArray.pop()); 
-console.log(stackWithArray.pop()); 
-console.log(stackWithArray.pop()); 
-console.log(stackWithArray.peek()); 
-
-// const myStack = new Stack()
-// console.log(myStack.push('google'));
-// console.log(myStack.push('udemy'));
-// console.log(myStack.push('amazon'));
-// console.log(myStack.pop());
-// console.log(myStack.pop());
-// console.log(myStack.pop());
-// console.log(myStack.peek());
+const myStack = new Stack()
+console.log(myStack.push('google'));
+console.log(myStack.push('udemy'));
+console.log(myStack.push('amazon'));
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.peek());
