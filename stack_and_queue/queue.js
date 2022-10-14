db81b8d684cb8e3699a024ad86ee80cc1194fd87
= <adedejifavour6@gmail.com>
2022-10-14 15:58:16 +0100

    Add queue data structure

---
+++ stack_and_queue/queue.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.first
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value)
+    if (this.length === 0) {
+      this.first = newNode
+      this.last = newNode
+    } else {
+      this.last.next = newNode
+      this.last = newNode
+    }
+    this.length++
+    return this; 
+  }
+
+  dequeue() {
+    if (!this.first) {
+      return null
+    }
+    if (this.first === this.last) {
+      this.last = null
+    }
+    this.first = this.first.next
+    this.length--
+    return this
+  }
+}
+
+const myQueue = new Queue()
+console.log(myQueue.enqueue('james'));
+console.log(myQueue.enqueue('peter'));
+console.log(myQueue.enqueue('john'));
+console.log(myQueue.peek());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
