247e1dbd00c87edba9f499d8a15aa81cd0cd76b9
= <adedejifavour6@gmail.com>
2022-10-14 15:21:15 +0100

    Solve stack using linked list

---
+++ stack_and_queue/stack.js
@@ -0,0 +1,60 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(value) {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  peek() {
+    if (this.length === 0) {
+      return null;
+    }
+
+    if (this.length > 0) {
+      return this.top;
+    }
+  }
+
+  push(value) {
+    const newNode = new Node(value)
+    if (!this.bottom) {
+      this.top = newNode
+      this.bottom = newNode
+    } else {
+      const holdingPointer = this.top
+    this.top = newNode
+    this.top.next = holdingPointer
+    }
+    this.length++
+    return this
+  }
+
+  pop() {
+    if (this.length === 0) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null
+    }
+    // const nextNode = this.top // to have reference to the last item removed
+    this.top = this.top.next
+    this.length--
+    return this
+  }
+}
+
+const myStack = new Stack(2)
+console.log(myStack.push('google'));
+console.log(myStack.push('udemy'));
+console.log(myStack.push('amazon'));
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.peek());
