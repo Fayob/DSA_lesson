323bbed321a2a9860986d9468541cfa4af9c10a1
= <adedejifavour6@gmail.com>
2022-10-28 20:42:18 +0100

    Add breadth first search functionality

---
+++ algorithms/searching/bfs.js
@@ -0,0 +1,106 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return this;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return this;
+        }
+        currentNode = currentNode.right;
+      }
+    }
+  }
+
+  lookup(value) {
+    if (this.root === null) {
+      return false;
+    }
+
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value === currentNode.value) {
+        return currentNode;
+      } else if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+    return "Not Found";
+  }
+
+  breadthFirstSearch() {
+    let currentNode = this.root
+    let list = []
+    let queue = []
+    queue.push(currentNode)
+
+    while (queue.length > 0) {
+      currentNode = queue.shift()
+      list.push(currentNode.value)
+
+      if (currentNode.left) {
+        queue.push(currentNode.left)
+      }
+
+      if (currentNode.right) {
+        queue.push(currentNode.right)
+      }
+    }
+    return list
+  }
+
+  breadthFirstSearchR(queue, list) {
+    if (!queue.length) {
+      return list
+    }
+    let currentNode = queue.shift()
+    list.push(currentNode.value)
+    if (currentNode.left) {
+        queue.push(currentNode.left)
+      }
+
+    if (currentNode.right) {
+      queue.push(currentNode.right)
+    }
+
+    return this.breadthFirstSearchR(queue, list)
+  }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(8);
+tree.insert(1);
+tree.insert(18);
+tree.insert(6);
+tree.insert(40);
+tree.insert(0);
+tree.insert(22);
+tree.lookup(40);
+console.log(tree.breadthFirstSearch());
+console.log(tree.breadthFirstSearch(this.root, []));
